bfa9496914a2e92f94e06162d4d39db595b28ed4
CMelina <allen.586@wright.edu>
2026-04-02 11:25:21 -0400

    Update server.js

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const fs = require('fs');
+const path = require('path');
 
+const app = express();
+const PORT = 3000;
+const LOG_FILE = 'system_logs.log';
+
+// Static folders
+app.use(express.static(__dirname));
+app.use('/plans', express.static(path.join(__dirname, 'received_plans')));
+
+app.get('/api/list-images', (req, res) => {
+    const imagesPath = path.join(__dirname, 'received_plans');
+    
+    if (!fs.existsSync(imagesPath)) {
+        return res.json([]);
+    }
+
+    fs.readdir(imagesPath, (err, files) => {
+        if (err) return res.status(500).json([]);
+        const images = files.filter(file => /\.(png|jpg|jpeg|gif|webp)$/i.test(file));
+        res.json(images);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server active on http://localhost:${PORT}`);
+});
